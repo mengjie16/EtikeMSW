9b8e52e3a1e0721dc7969c9f0680c43791e04db4
mengjie16 <564691062@qq.com>
2017-07-16 15:57:51 +0800

    myOrderHTML

--- public/javascripts/bizComponents/retailer/orderList.js
+++ public/javascripts/bizComponents/retailer/orderList.js
@@ -71,6 +71,9 @@ function initBase(){
 
 //加载订单列表
 function loadList(pageNo) {
+	if(!pageNo){
+		pageNo =1;
+	}
 	var obj = {
 		'vo.id':$('#orderNo').val(),
 		'vo.pageNo': pageNo,
@@ -137,10 +140,99 @@ function loadList(pageNo) {
 function loadListCallBack(index, jq) {
 	loadList(index + 1);
 }
-
+function initRestList(e){
+	var eve = e?e:(window.event?window.event:null);
+	var target = eve.target;
+	var id = target.id;
+	var tradeStatus;
+	var tabindex;
+	switch (id){
+		case 'toPay':
+			tradeStatus = 'TRADE_UNPAIED';
+			tabindex =2;
+			break;
+		case 'toSendGoods':
+			tradeStatus = 'TRADE_UNSEND';
+			tabindex =3;
+			break;
+		case 'toGetGodds':
+			tradeStatus = 'TRADE_UNRECIIVED';
+			tabindex=4;
+			break;
+	}
+	renderRestList(1,tradeStatus,tabindex);
+}
+function renderRestList(pageNo,tradeStatus,tabindex){
+	alert("hihi");
+	var obj = {
+		'vo.id':$('#orderNo').val(),
+		'vo.pageNo': pageNo,
+		'vo.pageSize': CDT.pageSize,
+		'vo.createTimeStart': $('#startTime').val(),
+		'vo.createTimeEnd': $('#endTime').val(),
+		'status':tradeStatus
+	};
+	Tr.get('/retailer/trade/queryByTradeStatus', obj, function(data) {
+/*		if (data.code != 200||!data.results) return;
+		// 保存到缓存
+		CDT.loadedListCache = data.results;*/
+		var output = Mustache.render(CDT.orderTemp, $.extend(data, {
+			'paymentStr':function(){
+				if(this.cargoFee){
+					return this.cargoFee/100;
+				}
+				return 0;
+			},
+			'shippingFeeStr':function(){
+				if(this.shippingFee){
+					return this.shippingFee/100;
+				}
+				return 0;
+			},
+			'totalAmountStr':function(){
+				if(this.totalFee){
+					return this.totalFee/100;
+				}
+				return 0;
+			},
+			'realityPaymentStr':function(){
+				if(this.payment){
+					return this.payment/100;
+				}
+				return 0;
+			},
+			'discountStr':function(){
+				if(this.discountFee){
+					return this.discountFee/100;
+				}
+				return 0;
+			}
+		}));
+		$('#listContainer'+tabindex).html(output);
+		if(data.totalCount<=0){
+			$('.pagination').hide();
+			return;
+		}
+		$('.pagination').show();
+		$('.pagination').pagination(data.totalCount, {
+			items_per_page: CDT.pageSize,
+			num_display_entries: 5,
+			current_page: pageNo,
+			num_edge_entries: 5,
+			callback: loadListCallBack,
+			callback_run: false,
+			prev_text:' '
+		});
+		CDT.currPageNo = pageNo;
+	}, {
+				loadingMask: true
+			});
+}
 $(function(){
 	CDT.orderTemp = $('#orderTemp').remove().val();
 	Mustache.parse(CDT.orderTemp);
-	initBase();
+	//initBase();
 	loadList(1);
+	$(document).on('click','#toPay,#toSendGoods,#toGetGodds',initRestList);
+	$(document).on('click','#allOrders',loadList);
 });
