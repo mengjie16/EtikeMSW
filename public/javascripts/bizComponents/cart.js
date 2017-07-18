c95e9be0a230e1438236e7bce01bd2cee41f8c0d
Meng <jenny.meng@sap.com>
2017-07-18 20:21:53 +0800

    cart

--- public/javascripts/bizComponents/cart.js
+++ public/javascripts/bizComponents/cart.js
@@ -32,19 +32,46 @@ $(function() {
                     $(this).next().next().removeClass('no-plus');
                     if ($(this).next()[0].value > 1) {
                         $(this).next()[0].value--;
+                        
                         if (parseInt($(this).next()[0].value) === 1) {
                             $(this).addClass('no-minus');
                         }
                     } else {
                         $(this).addClass('no-minus');
                     }
+
                 });
                 $('.J_Plus').on('click', function(e) {
-                    var storeNumber = 12;
+                    var storeNumber = 100;
                     $(this).prev().prev().removeClass('no-minus');
                     if ($(this).prev()[0].value < storeNumber) {
                         $(this).prev()[0].value++;
-                        console.log($('.text-amount').val())
+                        //以后如果需要实时更新数量可以用这个
+                        //有会话频繁问题
+                        /*var itemId = $(this).prev().data('id');
+                        var $line = $('tr#'+itemId);
+                        var $brandName = $line.find('.detailCart .brand').text();
+                        var $title = $line.find('.detailCart .title').text();
+                        var $color = $line.find('.detailCart .color').text();
+                        var $picUrl = $line.find('.detailCart img').attr('src');
+                        var $retailPrice = $line.find('.detailCart check_price').text().substring();
+                        var $cartCount =  $(this).prev()[0].value;
+                        var totalPriceRMB = $cartCount * $retailPrice;
+                        var params = {
+				            "itemId": itemId,
+				            "itemVo.sku.color":$color,
+				            "itemVo.picUrl" : $picUrl,
+				            "itemVo.cartCount" :$cartCount,
+				            "itemVo.retailPrice" : $retailPrice,
+				            "itemVo.cny2eur":0,
+				            "itemVo.cartPrice" : totalPriceRMB,
+				            "itemVo.title" : $title,
+				            "itemVo.brand.name" : $brandName
+				        };
+                        $.when(addCntUploadCart(params)).done(function(){
+	                    	$line.find('.detailCart perTotalPrice').text(totalPriceRMB);
+	                    });*/
+                        //console.log($('.text-amount').val())
                         if (parseInt($(this).prev()[0].value) === storeNumber) {
                             $(this).addClass('no-plus');
                         }
@@ -52,6 +79,7 @@ $(function() {
                         $(this).addClass('no-plus');
                     }
                 });
+
                 //删除购物车里的条目
                 $(document).on('click', '.delete', function() {
                     var id = $(this).data('id');
@@ -103,19 +131,20 @@ $(function() {
             if (CART.cartCache.length > 0) {
                 $('#cartContainer').empty();
                 $.each(CART.cartCache, function(index, obj) {
+                	var itemId= obj.id;
                 	var brandName = obj.brand.name;
                 	var cartCount = obj.cartCount;
                 	var picUrl = obj.picUrl;
                 	var retailPrice = obj.retailPrice;
                 	var title = obj.title;
                 	var color = obj.sku.color;
-                	totalPrice = cartCount*retailPrice;
-                    var $titledt = $("<td class='ring-in'><div class='cart-goods-checkbox'><input class='J_CheckBoxShop' type='checkbox' name='select-goods'  value='true'></div><div><a target='_blank' href='/item/" + obj.id + "' class='at-in'><img src='" + picUrl + "' class='img-responsive' alt=''></a><div class='sed'><p>" + brandName+title + "</p></div></div><div class='clearfix'></div></td>");
+                	var perTotalPrice = cartCount*retailPrice;
+                    var $titledt = $("<td class='ring-in'><div class='cart-goods-checkbox'><input class='J_CheckBoxShop' type='checkbox' name='select-goods'  value='true'></div><div><a target='_blank' href='/item/" + obj.id + "' class='at-in'><img src='" + picUrl + "' class='img-responsive' alt=''></a><div class='sed'><p class='detailCart><span class='brand'>"+brandName+"</span><span class='title'>"+title+"</span><span class='color'>"+color+"</span></p></div></div><div class='clearfix'></div></td>");
                     var $basedd = $("<td class='per-price'><div class='check_price'>¥" + retailPrice + "<span class='price-e'></span></div></td>");
-                    var $skudd = $("<td class='check'><div class='amount-wrapper '><div class='item-amount '><a class='J_Minus minus no-minus'>-</a><input id='cartCount' type='text' value='"+cartCount+"' class='text text-amount J_ItemAmount' data-max='50' data-now='2' autocomplete='off'><a class='J_Plus plus'>+</a></div><div class='amount-msg J_AmountMsg'></div></div></td>");
-                    var $totaldd = $("<td class='total-price'><div class='check_price allrmb'>¥<span class='price-e allou'>"+totalPrice+"</span></div></td>");
+                    var $skudd = $("<td class='check'><div class='amount-wrapper'><div class='item-amount '><a class='J_Minus minus no-minus updateVal'>-</a><input type='text' data-id='"+itemId+"' value='"+cartCount+"' class='cartCount text text-amount J_ItemAmount' data-max='50' data-now='2' autocomplete='off'><a class='updateVal J_Plus plus'>+</a></div><div class='amount-msg J_AmountMsg'></div></div></td>");
+                    var $totaldd = $("<td class='total-price'><div class='check_price allrmb'>¥<span class='price-e allou perTotalPrice'>"+perTotalPrice+"</span></div></td>");
                     var $fundd = $("<td data-id='" + obj.id + "' class='check-goodsdelete delete'>删除</td>");
-                    var $itemdl = $("<tr></tr>");
+                    var $itemdl = $("<tr class='lineCart' id='"+obj.id+"'></tr>");
                     $itemdl.append($titledt);
                     $itemdl.append($basedd);
                     $itemdl.append($skudd);
@@ -129,4 +158,22 @@ $(function() {
                 });
                 return dtd.resolve();
             }
+        }
+        function addCntUploadCart(obj){
+        	//var params = JSON.stringify(obj)
+        	var dtd=$.Deferred();
+        	var params= obj;
+        	Tr.post('/retailer/cart/add', params, function(data) {
+
+            if (data.code != 200) {
+                /*alert('购物车失败');*/
+                dtd.reject();
+            }else{
+            	dtd.resolve();
+            }
+
+
+        	});
+        	return dtd.promise();
+
         }
