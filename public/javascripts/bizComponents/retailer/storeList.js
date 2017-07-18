eded7669db341f6700f5bf191602a97cab1a33ee
kitty <243250131@qq.com>
2017-07-18 20:22:09 +0800

    收藏删除

--- public/javascripts/bizComponents/retailer/storeList.js
+++ public/javascripts/bizComponents/retailer/storeList.js
@@ -1,5 +1,21 @@
 function initBase(argument) {
-	
+	/*取消收藏*/
+	$(document).on('click', '.item_cancel', function() {
+		var mid = $(this).attr('mid');
+		if (!confirm('确定取消收藏该商品吗？')) {
+			return;
+		}
+		Tr.get('/', {
+			"id": mid
+		}, function(data) {
+			if (data.code != 200) {
+				alert(data.msg);
+				return;
+			}
+			alert('取消成功');
+			window.location.reload();
+		});
+	});
 }
 
 $(function() {
