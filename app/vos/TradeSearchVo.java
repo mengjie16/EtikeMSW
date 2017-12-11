df2e53128c6792dfaaa7d0a2cc2d1caf140c577f
maomao2 <tanxiazhe@163.com>
2017-12-11 16:13:01 +0800

    fix export excel bug

--- app/vos/TradeSearchVo.java
+++ app/vos/TradeSearchVo.java
@@ -12,7 +12,7 @@ import utils.SearchEndDateBinder;
  * 品牌信息检索
  * 
  * @author Calm
- * @since  v1.0
+ * @since v1.0
  * @created 2016年5月30日 下午6:23:33
  */
 public class TradeSearchVo extends Page {
@@ -21,14 +21,16 @@ public class TradeSearchVo extends Page {
     /** 交易创建时间 */
     @As("yyyy-MM-dd")
     public Date createTimeStart;
-    @As(binder=SearchEndDateBinder.class)
+    @As(binder = SearchEndDateBinder.class)
     public Date createTimeEnd;
     // 交易状态
     public TradeStatus status;
-	// 交易标题
-	public String caption;
-	// 零售商id
-	public int retailerId;
-	// 手机号
-	public String phone;
+    // 交易标题
+    public String caption;
+    // 零售商id
+    public int retailerId;
+    // 手机号
+    // public String phone;
+    // 姓名
+    public String name;
 }
